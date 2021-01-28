137287e92fdbc8cca6b1cdd1d5bbb3e839f65353
Hoseong Lee <ohpyupi@gmail.com>
2021-01-27 21:01:46 -0800

    Updated the unit tests of the api service module

--- src/service/api.test.js
+++ src/service/api.test.js
@@ -1,41 +1,51 @@
 import api from "./api.js";
 
 describe('DogApi.fetchRandomDogImgs', () => {
-    it('calls the fetch API and return the JSON data', async () => {
-        expect.assertions(1);
-        const mockSuccessResponse = {
-                "message": ["dog1img.jpeg"],
-                "status": "success"
-            };
-        const mockJsonPromise = Promise.resolve(mockSuccessResponse);
-        const mockFetchPromise = Promise.resolve({
-            json: () => mockJsonPromise,
-        });
-        jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
-
-        const data = await api.fetchRandomDogImgs(1)
-        expect(typeof data).toBe("object");
-        global.fetch.mockClear();
+    const mockFetchJson = jest.fn();
+    fetch = jest.fn();
+    afterEach(() => {
+        fetch.mockClear();
+        mockFetchJson.mockClear();
     });
+    it('calls the fetch API and return the JSON data', async () => {
+        expect.assertions(3);
+        const response = {
+            "message": ["dog1img.jpeg"],
+            "status": "success"
+        };
+        mockFetchJson.mockImplementation(async () => response);
+        fetch.mockImplementation(async () => ({
+            json: mockFetchJson,
+        }));
+        const data = await api.fetchRandomDogImgs(1)
+        expect(fetch).toHaveBeenCalled();
+        expect(mockFetchJson).toHaveBeenCalled();
+        expect(data).toEqual(response);
+    });
+    it.todo('throws an error in case the network call fails');
 });
 
 describe('DogApi.fetchDogBreeds', () => {
+    const mockFetchJson = jest.fn();
+    fetch = jest.fn();
+    afterEach(() => {
+        fetch.mockClear();
+        mockFetchJson.mockClear();
+    });
     it('calls the fetch API and return the JSON data', async () => {
-        expect.assertions(1);
-        const mockSuccessResponse = {
+        expect.assertions(3);
+        const response = {
             "message": {"affenpinscher": [], "cattledog": ["australian"]}, 
             "status": "success"
         };
-        const mockJsonPromise = Promise.resolve(mockSuccessResponse);
-        const mockFetchPromise = Promise.resolve({
-            json: () => mockJsonPromise,
-        });
-        jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
-
-        expect.assertions(1);
+        mockFetchJson.mockImplementation(async () => response);
+        fetch.mockImplementation(async () => ({
+            json: mockFetchJson,
+        }));
         const data = await api.fetchDogBreeds();
-        expect(typeof data).toBe("object");
-        global.fetch.mockClear();
+        expect(fetch).toHaveBeenCalled();
+        expect(mockFetchJson).toHaveBeenCalled();
+        expect(data).toEqual(response);
     });
+    it.todo('throws an error in case the network call fails');
 });
-
