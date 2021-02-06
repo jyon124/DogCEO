6a38a19dac2fc4412497a131debdfd12d9f0d181
Jooyon Won <jyon1204@gmail.com>
2021-02-05 22:00:22 -0800

    DogPage Unit Test Updated

--- src/pages/dog-page/dog-page.test.js
+++ src/pages/dog-page/dog-page.test.js
@@ -3,8 +3,8 @@ import DogPage from "./dog-page.js";
 import DogCard from "../../components/dog-card/dog-card.js";
 import Loading from "../../components/loading/loading.js";
 import api from "../../service/api.js";
-import { mount, shallow } from "enzyme";
-import { waitFor } from "@testing-library/react";
+import { mount } from "enzyme";
+import { waitFor, fireEvent } from "@testing-library/react";
 
 describe("DogPage", () => {
     jest.mock("../../service/api", () => {
@@ -38,13 +38,14 @@ describe("DogPage", () => {
     });
     
     it("renders loading component if the fetch call is being made", async() => {
-        expect.assertions(1);
+        expect.assertions(2);
         api.fetchRandomDogImgs = jest.fn(() => ({
             message: ["dog1.jpg"],
             status: "success"
         }));
         const wrapper = mount(<DogPage />);
         await waitFor(() => {
+            expect(api.fetchRandomDogImgs).toHaveBeenCalled();
             expect(wrapper.containsMatchingElement(<Loading />)).toEqual(true);
         });
     });
@@ -63,16 +64,33 @@ describe("DogPage", () => {
     });
 
     it("renders only unique dog image - filters the duplicates", async () => {
-        // expect.assertions(1);
         api.fetchRandomDogImgs = jest.fn(() => ({
             message: ["dog1.jpg", "dog1.jpg", "dog3.jpg", "dog2.jpg", "dog3.jpg", "dog1.jpg"],
             status: "success"
         }));
         const wrapper = mount(<DogPage />);
-        await waitFor(async () => {
-            expect(wrapper.html()).toEqual(true);
+        await waitFor(() => {
+            expect(wrapper.html().split("src").length-1).toEqual(3);
         });
     });
-    it.todo("fetches additional dog images when the scroll reaches to the bottom and renders them");
+
+    it.skip("fetches additional dog images when the scroll reaches to the bottom and renders them" , async() => {
+        api.fetchRandomDogImgs = jest.fn(() => ({
+            message: [
+                "dog1.jpg", "dog2.jpg", "dog3.jpg", "dog4.jpg", "dog5.jpg", 
+                "dog6.jpg","dog7.jpg","dog8.jpg","dog9.jpg","dog10.jpg",
+                "dog11.jpg","dog12.jpg","dog13.jpg","dog14.jpg","dog15.jpg",
+                "dog16.jpg","dog17.jpg","dog18.jpg","dog19.jpg","dog20.jpg"
+            ],
+            status: "success"
+        }));
+        const wrapper = mount(<DogPage />);
+        fireEvent.scroll(global, { target: { scrollY: 2000 } });
+        await waitFor(() => {
+            // console.log(window.scrollY)
+            expect(api.fetchRandomDogImgs).toHaveBeenCalledWith(1);
+            // expect(wrapper.html()).toEqual(true);
+        })
+    });
     it.todo("tests the debounce allows the additional dog image fetch per certain time period");
 });
