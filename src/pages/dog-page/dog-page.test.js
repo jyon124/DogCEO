41cf59c22390f79a81bba7bc00f9b3ecae1da53c
Jooyon Won <jyon1204@gmail.com>
2021-02-12 19:59:18 -0800

    Updated DogPage Unit Test

--- src/pages/dog-page/dog-page.test.js
+++ src/pages/dog-page/dog-page.test.js
@@ -4,58 +4,83 @@ import DogCard from "../../components/dog-card/dog-card.js";
 import Loading from "../../components/loading/loading.js";
 import api from "../../service/api.js";
 import { mount } from "enzyme";
-import { waitFor } from "@testing-library/react";
+import { waitFor, fireEvent, act } from "@testing-library/react";
 
 jest.mock("../../service/api.js");
-const dogImgResp = {
-    message: ["dog1.jpg", "dog1.jpg", "dog3.jpg", "dog2.jpg", "dog3.jpg", "dog1.jpg"],
-    status: "success"
-};
-const dogBreedsResp = {
-    message: { affenpinscher: [], cattledog: ["australian"] },
-    status: "success",
-};
 
 describe("DogPage", () => {
-    beforeEach(() => {
-        api.fetchRandomDogImgs.mockImplementationOnce(() => dogImgResp);
-        api.fetchDogBreeds.mockImplementationOnce(() => dogBreedsResp);
-    });
-
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    it("renders loading component for the initial render", async () => {
-        // On mount, DogPage invoke fetch requests in useEffect that spin up loading
+    it("Renders loading component for the initial render", async () => {
+        expect.assertions(1);
+        const dogImgResp = {
+            message: ["dog1.jpg"],
+            status: "success"
+        };
+        const dogBreedsResp = {
+            message: { affenpinscher: [], cattledog: ["australian"] },
+            status: "success",
+        };
+        api.fetchRandomDogImgs.mockImplementationOnce(() => dogImgResp);
+        api.fetchDogBreeds.mockImplementationOnce(() => dogBreedsResp);
         const wrapper = mount(<DogPage />);
         await waitFor(() => {
             expect(wrapper.containsMatchingElement(<Loading />)).toEqual(true);
         })
     });
 
-    it("renders loading component if no dog images are fetched", async () => {
+    it("Renders loading component if no dog images are fetched", async () => {
         expect.assertions(2);
+        const dogImgResp = {
+            message: ["dog1.jpg"],
+            status: "success"
+        };
+        const dogBreedsResp = {
+            message: { affenpinscher: [], cattledog: ["australian"] },
+            status: "success",
+        };
+        api.fetchRandomDogImgs.mockImplementationOnce(() => dogImgResp);
+        api.fetchDogBreeds.mockImplementationOnce(() => dogBreedsResp);
         const wrapper = mount(<DogPage />);
         await waitFor(() => {
-            // If there are no DogCard component then check existence of Loading component 
             expect(wrapper.containsAnyMatchingElements(DogCard)).toEqual(false);
             expect(wrapper.containsMatchingElement(<Loading />)).toEqual(true);
         });
     });
     
-    it("renders loading component if the fetch call is being made", async () => {
+    it("Renders loading component if the fetch call is being made", async () => {
         expect.assertions(2);
+        const dogImgResp = {
+            message: ["dog1.jpg"],
+            status: "success"
+        };
+        const dogBreedsResp = {
+            message: { affenpinscher: [], cattledog: ["australian"] },
+            status: "success",
+        };
+        api.fetchRandomDogImgs.mockImplementationOnce(() => dogImgResp);
+        api.fetchDogBreeds.mockImplementationOnce(() => dogBreedsResp);
         const wrapper = mount(<DogPage />);
         await waitFor(() => {
-            // expect(wrapper.html()).toEqual(1);
             expect(api.fetchRandomDogImgs).toHaveBeenCalled();
             expect(wrapper.containsMatchingElement(<Loading />)).toEqual(true);
         });
     });
 
-    it("renders DogCard components when dog images data are available", async () => {
+    it("Renders DogCard components when dog images data are available", async () => {
         expect.assertions(2);
+        const dogImgResp = {
+            message: ["dog1.jpg"],
+            status: "success"
+        };
+        const dogBreedsResp = {
+            message: { affenpinscher: [], cattledog: ["australian"] },
+            status: "success",
+        };
+        api.fetchRandomDogImgs.mockImplementationOnce(() => dogImgResp);
+        api.fetchDogBreeds.mockImplementationOnce(() => dogBreedsResp);
         const wrapper = mount(<DogPage />);
         await waitFor(() => {
             expect(api.fetchRandomDogImgs).toHaveBeenCalled();
@@ -63,13 +88,50 @@ describe("DogPage", () => {
         });
     });
 
-    it("renders only unique dog image - filters the duplicates", async () => {
+    it("Renders only unique dog image - filters the duplicates", async () => {
+        const dogImgResp = {
+            message: ["dog1.jpg", "dog1.jpg", "dog3.jpg", "dog2.jpg", "dog3.jpg", "dog1.jpg"],
+            status: "success"
+        };
+        const dogBreedsResp = {
+            message: { affenpinscher: [], cattledog: ["australian"] },
+            status: "success",
+        };
+        api.fetchRandomDogImgs.mockImplementationOnce(() => dogImgResp);
+        api.fetchDogBreeds.mockImplementationOnce(() => dogBreedsResp);
         const wrapper = mount(<DogPage />);
         await waitFor(() => {
             expect(wrapper.html().split("src").length-1).toEqual(3);
         });
     });
 
-    it.todo("fetches additional dog images when the scroll reaches to the bottom and renders them");
+    it("Fetches additional dog images when the scroll reaches to the bottom and renders them", async () => {
+        const dogImgResp = {
+            message: ["dog1.jpg"],
+            status: "success"
+        };
+        const secondDogImgResp = {
+            message: ["dog6.jpg", "dog7.jpg", "dog8.jpg"],
+            status: "success" 
+        };
+        const dogBreedsResp = {
+            message: { affenpinscher: [], cattledog: ["australian"] },
+            status: "success",
+        };
+        api.fetchRandomDogImgs
+            .mockImplementationOnce(() => dogImgResp)
+            .mockImplementationOnce(() => secondDogImgResp);
+        api.fetchDogBreeds.mockImplementationOnce(() => dogBreedsResp);
+        const height = window.innerHeight;
+        const wrapper = mount(<DogPage />);
+        act(() => {
+            fireEvent.scroll(window, { target: { scrollY: height } });
+        });
+        await waitFor(() => {
+            expect(api.fetchRandomDogImgs).toHaveBeenCalledTimes(2);
+            expect(wrapper.html()).toEqual(true);
+        });
+    });
+
     it.todo("tests the debounce allows the additional dog image fetch per certain time period");
 });
